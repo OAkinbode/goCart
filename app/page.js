cdd34b321502e8feea0a7c5f93d60d9ac036f041
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-05-16 10:33:03 -0600

    pushing a new next.js application

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-teal-100 w-full flex flex-col justify-center items-center">
+      <Header />
+      <div className="bg-gray-50 w-4/5 flex flex-col justify-center items-center py-2 min-h-screen">
+        Hello Again. Welcome to wedbev class
+      </div>
+      <Footer />
+    </main>
+  );
+}
