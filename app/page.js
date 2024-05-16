c1511d3ae06111230803e8b7fa54b160e4207ac1
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-05-16 11:19:53 -0600

    changes made

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
     <main className="min-h-screen bg-teal-100 w-full flex flex-col justify-center items-center">
       <Header />
       <div className="bg-gray-50 w-4/5 flex flex-col justify-center items-center py-2 min-h-screen">
-        Hello Again. Welcome to wedbev class
+        Hello Again. Welcome to wedbev class. I am changing a bunch of things to
+        demonstrate ci/cd. Continous integration, continous deployment.
       </div>
       <Footer />
     </main>
